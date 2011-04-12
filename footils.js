606a349bb23d7c414f49e1d75538c340913c5c55
Rick Waldon <rick@bocoup.com>
2011-04-12 09:46:37 -0400

    Initial commit; no implementation

---
+++ footils.js
@@ -0,0 +1,23 @@
+(function () {
+
+	//	Create localized reference to global context
+	var global = this,
+
+	// Set up our "util" module
+			Footils = (function () {
+
+				//	normally, we might do something important in here
+				return {
+
+					forEach: function( obj, fn, context ) {
+
+					}
+
+				};
+
+
+			})();
+
+	global.Footils = global.F = Footils;
+})();
+
